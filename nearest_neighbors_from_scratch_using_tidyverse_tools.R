90502d29f8d8df53c18fc49c5178424de68ef0ec
Amarullazo <julio.collazos@unmsm.edu.pe>
2022-11-28 00:50:49 -0500

    repaired algorithm by Julio

--- nearest_neighbors_from_scratch_using_tidyverse_tools.R
+++ nearest_neighbors_from_scratch_using_tidyverse_tools.R
@@ -1,8 +1,7 @@
 library(tidyverse)
 library(palmerpenguins)
 library(janitor)
-library(cluster) # This library have the function daisy() which we will use to
-# calculate the distances between points.
+library(smotefamily)
 
 penguins |> 
   mutate(key = str_c("id_", 1:344)) |> 
@@ -10,66 +9,63 @@ penguins |>
 # Let's just match with two variables "flipper_length_mm" and "flipper_length_mm"
 penguins |> 
   select(key, bill_length_mm,  flipper_length_mm) -> penguins_sapales
-# Need to scale the variables so that the method doesn't render ridiculous outcomes.
-penguins_sapales |> 
-  mutate(across(.cols = where(is.numeric),
-                .fns = scale)) |> 
-  drop_na() -> penguins_sapales
-
-neighbours_to_match <-  3 # neighbors to match to each point. (3 is just an example,
-# you can add any other number)
-
-penguins_sapales |> 
-  mutate(
-    original_tibble = list(penguins_sapales) # Add the original tibble to each point.
-  ) |> 
-  mutate( # Add the point of each row to a new nested tibble called "row_point"
-    row_point = pmap(
-      list(.x = bill_length_mm, .y = flipper_length_mm, 
-           .z = key),
-      .f = function(.x, .y, .z){
-        tibble(
-          key = .z,
-          bill_length_mm = .x,
-          flipper_length_mm = .y
-        )
-      })
-    )|> 
-  # Let's prune the original tibble, so that it can be separated from the point
-  # we want to match (That's why we use anti_join).
-  mutate(
-    original_tibble = map2(.x = original_tibble, .y = row_point,
-                .f = function(.x, .y){
-                  .x |> 
-                    anti_join(.y, by = "key")
-                })
-  )  |> 
-  mutate(
-    original_tibble = map(original_tibble, function(original_tibble){
-      original_tibble |> select(-key)
-    }),
-    row_point = map(row_point, function(row_point){
-      row_point |> select(-key)
-    })
-  ) |> 
-  select(-c(bill_length_mm, flipper_length_mm)) |> 
-  mutate(both = map2(.x = row_point, .y = original_tibble,
-                     .f = function(.x, .y){
-                       .x |> 
-                         bind_rows(.y) # Needed for the function daisy() 
-  # All points must be on the same data.frame AND we need the row_point to be on the top.
-                     })) |> 
-  mutate(distance = map(.x = both, .f = function(.x){
-    .x |> as.matrix() |> daisy(metric = "euclidean") 
-  })) |> 
-  mutate(neighbours = map(.x = distance, .f = function(.x){
-    .x |> as.matrix() |> 
-      as.data.frame() |> 
-      rownames_to_column() |> 
-      as_tibble() |> 
-      filter(rowname != 1) |>  # First row is just the distance between the point with itself
-      janitor::clean_names() |> 
-      arrange(x1) |> 
-      slice(1:neighbours_to_match) # This is the key.
-  })) 
+#-----------------------------------
+tidy_nearest <- function(.data, nearest_neighbours = 5,
+                         key = "key"){
+  .data <- .data |> drop_na() |> 
+    mutate(across(.cols = where(is.numeric),
+                  .fns = function(x){
+                    (x - mean(x))/sd(x)
+                  })) # scale the numeric variables, FIRST
+  data_sapal <- .data |> 
+    drop_na() |> 
+    column_to_rownames(key) |> 
+    as_tibble() 
   
+  table_to_match <- data_sapal |> 
+    as_tibble() |> 
+    rownames_to_column(key)
+  
+  resultados_nearest_neighbour <- knearest(data_sapal,
+           data_sapal,
+           n_clust = nearest_neighbours) # STEP 1: Get the index of each row(knn)
+  resultados <- resultados_nearest_neighbour |> 
+    as_tibble() |> 
+    rownames_to_column("key")  |> 
+    nest(all_keys = -key) |>  # STEP 2
+    mutate(all_keys = map(all_keys,
+                            .f = function(all_keys){
+                              all_keys |> unlist() |> 
+                                unname()
+                            })) |> 
+    select(key, all_keys)
+  resultados_finales <- table_to_match |> 
+    left_join(resultados, by = "key") |> # STEP 3
+    mutate(table_to_match = list(table_to_match)) 
+  resultados_finales_2 <- resultados_finales |> 
+    mutate(
+      resultado_final = pmap(list(all_keys,
+                                  table_to_match,
+                                  key),
+                             .f = function(all_keys, table_to_match,
+                                           key){
+                               keys_to_match <- c(all_keys, key)
+                               table_to_match |> 
+                                 filter(key %in% c(keys_to_match))
+                             }) 
+    )
+  resultados_finales_2 |> 
+    select(key, resultado_final)
+}
+all_keys
+
+# DEMOSTRACIÓN
+
+tidy_nearest(
+  .data = penguins_sapales,
+  nearest_neighbours = 3,
+  key = "key"
+) -> resultados_finalisimos
+
+resultados_finalisimos |> pluck("resultado_final", 3)
+
